bc169e70697522b84470b16f1a0a55410c5fb845
Tanita <hruzina_t@mail.ru>
2023-09-01 00:08:22 +0300

    Модуль 9. Урок 228-229. Валидация вводимых данных в форму ввода данных.

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -30,13 +30,26 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   void _updateImageUrl() {
     if (!_imageUrlFocusNode.hasFocus) {
+      if ((!_imageUrlController.text.startsWith('http') &&
+              !_imageUrlController.text.startsWith('https')) ||
+          (!_imageUrlController.text.endsWith('.jpg') &&
+              !_imageUrlController.text.endsWith('.jpeg') &&
+              !_imageUrlController.text.endsWith('.png'))) {
+        print('СРАБОТАЛО!!!!!!!!!!!!!');
+        return;
+      }
       setState(() {});
     }
   }
 
   void _saveForm() {
+    final isValid = _form.currentState?.validate();
+    if (isValid != null && !isValid) {
+      return;
+    }
     _form.currentState?.save();
-    print('--------------------------------');
+    print('------------------------------------------------------');
+    print('------------------------------------------------------');
     print('title: ${_editedProduct.title}');
     print('description: ${_editedProduct.description}');
     print('price: ${_editedProduct.price}');
@@ -63,7 +76,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
         title: const Text('Edit Product'),
         actions: [
           IconButton(
-            icon: Icon(Icons.save),
+            icon: const Icon(Icons.save),
             onPressed: _saveForm,
           )
         ],
@@ -71,117 +84,160 @@ class _EditProductScreenState extends State<EditProductScreen> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
-            key: _form,
-            child: ListView(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Title'),
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_priceFocusNode);
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      _editedProduct = Product(
-                        id: _editedProduct.id,
-                        title: newValue,
-                        description: _editedProduct.description,
-                        price: _editedProduct.price,
-                        imageUrl: _editedProduct.imageUrl,
-                      );
-                    }
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Price'),
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.number,
-                  focusNode: _priceFocusNode,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_descriptionFocusNode);
-                  },
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      _editedProduct = Product(
-                        id: _editedProduct.id,
-                        title: _editedProduct.title,
-                        description: _editedProduct.description,
-                        price: double.parse(newValue),
-                        imageUrl: _editedProduct.imageUrl,
-                      );
-                    }
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Description'),
-                  maxLines: 3,
-                  // textInputAction: TextInputAction.next,
-                  // textInputAction убираем, так как всё равно при мультистрочном вводе
-                  // он не будет работать
-                  keyboardType: TextInputType.multiline,
-                  focusNode: _descriptionFocusNode,
-                  onSaved: (newValue) {
-                    if (newValue != null) {
-                      _editedProduct = Product(
-                        id: _editedProduct.id,
-                        title: _editedProduct.title,
-                        description: newValue,
-                        price: _editedProduct.price,
-                        imageUrl: _editedProduct.imageUrl,
-                      );
-                    }
-                  },
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      margin: EdgeInsets.only(top: 8, right: 10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1,
-                          color: Colors.grey,
-                        ),
+          key: _form,
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return 'Введите название!';
+                  }
+                  return null; // Означает, что нет ошибки
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    _editedProduct = Product(
+                      id: _editedProduct.id,
+                      title: newValue,
+                      description: _editedProduct.description,
+                      price: _editedProduct.price,
+                      imageUrl: _editedProduct.imageUrl,
+                    );
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return 'Введите цену!';
+                  }
+                  if (value != null && double.tryParse(value) == null) {
+                    return 'Введите цену правильно!';
+                  }
+                  if (value != null && double.parse(value) <= 0) {
+                    return 'Введите положительное число!';
+                  }
+                  return null; // Означает, что нет ошибки
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    _editedProduct = Product(
+                      id: _editedProduct.id,
+                      title: _editedProduct.title,
+                      description: _editedProduct.description,
+                      price: double.parse(newValue),
+                      imageUrl: _editedProduct.imageUrl,
+                    );
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                // textInputAction: TextInputAction.next,
+                // textInputAction убираем, так как всё равно при мультистрочном вводе
+                // он не будет работать
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return 'Введите описание!';
+                  }
+                  if (value != null && value.length < 10) {
+                    return 'Описание должно быть более 10 символов';
+                  }
+                  return null; // Означает, что нет ошибки
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    _editedProduct = Product(
+                      id: _editedProduct.id,
+                      title: _editedProduct.title,
+                      description: newValue,
+                      price: _editedProduct.price,
+                      imageUrl: _editedProduct.imageUrl,
+                    );
+                  }
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(top: 8, right: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
                       ),
-                      child: _imageUrlController.text.isEmpty
-                          ? Text('Enter a URL')
-                          : FittedBox(
-                              child: Image.network(
-                                _imageUrlController.text,
-                                fit: BoxFit.cover,
-                              ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? const Text('Enter a URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageUrlController.text,
+                              fit: BoxFit.cover,
                             ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                      onFieldSubmitted: (_) => _saveForm(),
+                      // onEditingComplete: () {
+                      //   setState(() {});
+                      // },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Введите URL картинки!';
+                        }
+                        if (!value.startsWith('http') &&
+                            !value.startsWith('https')) {
+                          return 'Пожалуйста введите корректный адрес картинки!';
+                        }
+                        if (!value.endsWith('.jpg') &&
+                            !value.endsWith('.jpeg') &&
+                            !value.endsWith('.png')) {
+                          return 'Пожалуйста введите корректный тип картинки!';
+                        }
+                        return null; // Означает, что нет ошибки
+                      },
+                      onSaved: (newValue) {
+                        if (newValue != null) {
+                          _editedProduct = Product(
+                            id: _editedProduct.id,
+                            title: _editedProduct.title,
+                            description: _editedProduct.description,
+                            price: _editedProduct.price,
+                            imageUrl: newValue,
+                          );
+                        }
+                      },
                     ),
-                    Expanded(
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: 'Image URL'),
-                        keyboardType: TextInputType.url,
-                        textInputAction: TextInputAction.done,
-                        controller: _imageUrlController,
-                        focusNode: _imageUrlFocusNode,
-                        onFieldSubmitted: (_) => _saveForm(),
-                        // onEditingComplete: () {
-                        //   setState(() {});
-                        // },
-                        onSaved: (newValue) {
-                          if (newValue != null) {
-                            _editedProduct = Product(
-                              id: _editedProduct.id,
-                              title: _editedProduct.title,
-                              description: _editedProduct.description,
-                              price: _editedProduct.price,
-                              imageUrl: newValue,
-                            );
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
